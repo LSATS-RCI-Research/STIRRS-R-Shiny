9b778e9a0dc88c95a62158d04e64256b2b435262
Joe F Bauer <joebauer@umich.edu>
2021-08-23 15:38:18 -0400

    Upload New File

---
+++ app-root/omekatest/omekatest_ui.R
@@ -0,0 +1,31 @@
+# Rely on the 'WorldPhones' dataset in the datasets
+# package (which generally comes preloaded).
+library(datasets)
+library(httr)
+library(jsonlite)
+
+res = GET("https://stirrs.rll.lsa.umich.edu/api/items")
+# Use a fluid Bootstrap layout
+fluidPage(    
+  
+  # Give the page a title
+  titlePanel(res),
+  
+  # Generate a row with a sidebar
+  sidebarLayout(      
+    
+    # Define the sidebar with one input
+    sidebarPanel(
+      selectInput("region", "Region:", 
+                  choices=colnames(WorldPhones)),
+      hr(),
+      helpText("Data from AT&T (1961) The World's Telephones.")
+    ),
+    
+    # Create a spot for the barplot
+    mainPanel(
+      plotOutput("phonePlot")  
+    )
+    
+  )
+)
