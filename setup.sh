9bb5d33902294dd3a6455053462f95f569e940f1
Joe F Bauer <joebauer@umich.edu>
2021-09-07 11:37:49 -0400

    updating the package install code

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 # use this script to install any needed R libraries
 
 # joebauer 8-23-2021: adding two packages in order to try and get data out of Omeka's API. 
-install.packages(c("httr", "jsonlite"))
+install.packages(c("httr", "jsonlite"), repos="https://repo.miserver.it.umich.edu/cran/")
