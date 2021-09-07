6649e014431f415a0d87cdeab2bf27e4763cc971
Joe F Bauer <joebauer@umich.edu>
2021-09-07 12:21:22 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,7 @@ echo "---installing R packages---"
 #old version that is just R script
 #install.packages(c("httr", "jsonlite"), repos="https://repo.miserver.it.umich.edu/cran/")
 # try the bash version instead:
-Rscript -e 'install.packages(c("httr", "jsonlite"), repos="https://repo.miserver.it.umich.edu/cran/")'
+Rscript -e 'install.packages("httr", repos="https://repo.miserver.it.umich.edu/cran/")'
+Rscript -e 'install.packages("jsonlite", repos="https://repo.miserver.it.umich.edu/cran/")'
 echo "---installed R packages---"
+touch complete.txt
